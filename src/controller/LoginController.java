46a1d591747485fcd5eac2ef0b4678d737e7a5a4
Ze3non7 <fardeensayed21@gmail.com>
2024-09-22 15:56:27 +0530

    Login Completed. Along with redirection to dashboard.

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,16 +1,32 @@
 package controller;
 
+import db.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class LoginController {
+    @FXML
+    TextField tf_email;
+    @FXML
+    PasswordField tf_password;
+    @FXML
+    AnchorPane pane;
 
     //Changing Scene in Login Page (i.e. Redirecting User to Register Page)
     private Stage stage;
@@ -24,5 +40,46 @@ public class LoginController {
         stage.setScene(scene);
         stage.setTitle("Register");
         stage.show();
+    }
+
+    public void button_login(ActionEvent event) throws IOException
+    {
+        try{
+            String email = tf_email.getText();
+            String password = tf_password.getText();
+
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from users where email_id=? and password=?");
+
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                Parent root = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("../view/dashboard.fxml")));
+
+                Scene scene = new Scene(root);
+
+                Stage primaryStage = (Stage) pane.getScene().getWindow();
+                primaryStage.setTitle("Dashboard");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+                primaryStage.centerOnScreen();
+            }
+
+            else {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Username or Password does not match");
+                alert.showAndWait();
+
+                tf_email.clear();
+                tf_password.clear();
+                tf_email.requestFocus();
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
     }
 }
