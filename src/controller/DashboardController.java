f6b808b397833f949460186d8219846aa7204340
Ze3non7 <fardeensayed21@gmail.com>
2024-09-30 23:18:47 +0530

    Redirection to Repository Screen

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class DashboardController {
+
+    public void switchToRepo(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/repo.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Repository");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
