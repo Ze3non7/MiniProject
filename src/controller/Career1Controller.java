7eb4635ce62015d01d258ec0b3fd4a22262ecb8f
Adsay <ahmedsayedmails@gmail.com>
2024-10-20 11:33:22 +0530

    added repo design added functionality to career suggestor button on dashboard and linked the next buttons to next pages

--- src/controller/Career1Controller.java
+++ src/controller/Career1Controller.java
@@ -11,13 +11,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class DashboardController {
+public class Career1Controller {
     @FXML
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void careerConnect(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/career1.fxml")));
+
+    public void next1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/career2.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
--- src/controller/Career4Controller.java
+++ src/controller/Career4Controller.java
@@ -11,13 +11,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class DashboardController {
+public class Career4Controller {
     @FXML
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void careerConnect(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/career1.fxml")));
+
+    public void finish(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
